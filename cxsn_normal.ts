ec2bd0b780e71daa9f406a3e7dd73598e97b9d1c
Tqy-assassin <38372515+Tqy-assassin@users.noreply.github.com>
2021-08-19 14:32:31 +0800

    新增中文翻译

--- cxsn_normal.ts
+++ cxsn_normal.ts
@@ -38,98 +38,100 @@ enum Serial_mode {
     _readwrite = 3
 }
 
-//% block="cxsn_normal" color="#FA0010" weight=120 icon="\uf11b"
-namespace CXSN_normal {
-    export class cxsn_serial {
-        _mode: Serial_mode = Serial_mode._unused;
-        _rx_pin: DigitalPin;
-        _tx_pin: DigitalPin;
-        _delay: number = 0;
-        _recvDate: string = "";
-        _readable: boolean = false;
+class cxsn_serial {
+    _mode: Serial_mode = Serial_mode._unused;
+    _rx_pin: DigitalPin;
+    _tx_pin: DigitalPin;
+    _delay: number = 0;
+    _recvDate: string = "";
+    _readable: boolean = false;
 
-        constructor() {}
+    constructor() { }
 
-        public init(tx: DigitalPin, rx: DigitalPin, baud: number, mode: Serial_mode) {
-            this._rx_pin = tx;
-            this._tx_pin = rx;
-            this._mode = mode;
-            this._delay = 1000000 / baud;
-            this._readable = false;
-        }
+    public init(tx: DigitalPin, rx: DigitalPin, baud: number, mode: Serial_mode) {
+        this._rx_pin = tx;
+        this._tx_pin = rx;
+        this._mode = mode;
+        this._delay = 1000000 / baud;
+        this._readable = false;
+    }
 
-        public reInit(tx: DigitalPin, rx: DigitalPin, baud: number, mode: Serial_mode) {
-            this._rx_pin = tx;
-            this._tx_pin = rx;
-            this._mode = mode;
-            this._delay = 1000000 / baud;
-            this._readable = false;
-        }
+    public reInit(tx: DigitalPin, rx: DigitalPin, baud: number, mode: Serial_mode) {
+        this._rx_pin = tx;
+        this._tx_pin = rx;
+        this._mode = mode;
+        this._delay = 1000000 / baud;
+        this._readable = false;
+    }
 
-        public begin() {
-            if ((this._mode | Serial_mode._read) != 0) {
-                control.inBackground(() => this.reading())
-            }
-        }
-
-        private reading() {
-            if ((this._mode | Serial_mode._read) != 0) {
-                if (pins.digitalReadPin(this._rx_pin)) {
-                    let recvBuf: number = 0;
-                    control.waitMicros(this._delay);
-                    for (let i = 0; i < 8; i++) {
-                        if (pins.digitalReadPin(this._rx_pin)) {
-                            recvBuf |= (1 << i)
-                        } else {
-                            recvBuf &= ~(1 << i)
-                        }
-                        control.waitMicros(this._delay);
-                    }
-                    this._recvDate = String.fromCharCode(recvBuf);
-                    this._readable = true;
-                }
-            }
-        }
-
-        private writing(transDate: number) {
-            if ((this._mode | Serial_mode._write) != 0) {
-                pins.digitalWritePin(this._tx_pin, 0);
-                control.waitMicros(this._delay);
-                for (let i = 0; i < 8; i++) {
-                    if ((transDate & 0x01) == 0) {
-                        pins.digitalWritePin(this._tx_pin, 0);
-                    } else {
-                        pins.digitalWritePin(this._tx_pin, 1);
-                    }
-                    control.waitMicros(this._delay);
-                    transDate >>= 1;
-                }
-                pins.digitalWritePin(this._tx_pin, 1);
-            }
-        }
-
-        public write_string(msg: string) {
-            for (let i = 0; i < msg.length; i++) {
-                this.writing(msg[i].charCodeAt(0))
-            }
-        }
-        public write_numbers(nums: number[]) {
-            for (let i = 0; i < nums.length; i++) {
-                this.writing(nums[i])
-            }
-        }
-        public read(): string {
-            let res = this._recvDate;
-            this._recvDate = ""
-            this._readable = false
-            return res;
-        }
-        public aviable(): boolean {
-            return this._readable;
+    public begin() {
+        if ((this._mode | Serial_mode._read) != 0) {
+            control.inBackground(() => this.reading())
         }
     }
-    
-    let cx_serial: cxsn_serial[] = [new cxsn_serial(), new cxsn_serial(), new cxsn_serial(),new cxsn_serial(), new cxsn_serial(), new cxsn_serial()];
+
+    private reading() {
+        if ((this._mode | Serial_mode._read) != 0) {
+            if (pins.digitalReadPin(this._rx_pin)) {
+                let recvBuf: number = 0;
+                control.waitMicros(this._delay);
+                for (let i = 0; i < 8; i++) {
+                    if (pins.digitalReadPin(this._rx_pin)) {
+                        recvBuf |= (1 << i)
+                    } else {
+                        recvBuf &= ~(1 << i)
+                    }
+                    control.waitMicros(this._delay);
+                }
+                this._recvDate = String.fromCharCode(recvBuf);
+                this._readable = true;
+            }
+        }
+    }
+
+    private writing(transDate: number) {
+        if ((this._mode | Serial_mode._write) != 0) {
+            pins.digitalWritePin(this._tx_pin, 0);
+            control.waitMicros(this._delay);
+            for (let i = 0; i < 8; i++) {
+                if ((transDate & 0x01) == 0) {
+                    pins.digitalWritePin(this._tx_pin, 0);
+                } else {
+                    pins.digitalWritePin(this._tx_pin, 1);
+                }
+                control.waitMicros(this._delay);
+                transDate >>= 1;
+            }
+            pins.digitalWritePin(this._tx_pin, 1);
+        }
+    }
+
+    public write_string(msg: string) {
+        for (let i = 0; i < msg.length; i++) {
+            this.writing(msg[i].charCodeAt(0))
+        }
+    }
+    public write_numbers(nums: number[]) {
+        for (let i = 0; i < nums.length; i++) {
+            this.writing(nums[i])
+        }
+    }
+    public read(): string {
+        let res = this._recvDate;
+        this._recvDate = ""
+        this._readable = false
+        return res;
+    }
+    public aviable(): boolean {
+        return this._readable;
+    }
+}
+
+//% block="cxsn_normal" color="#F02010" weight=100 icon="\uf11b"
+//% groups="['motor']"
+namespace CXSN_normal {
+    let cx_serial: cxsn_serial[] = [new cxsn_serial(), new cxsn_serial(), new cxsn_serial(),
+                                    new cxsn_serial(), new cxsn_serial(), new cxsn_serial()];
     // for(let i = 0;i < 6;i++){
     //     cx_serial[i] = new cxsn_serial();
     // }
@@ -148,13 +150,15 @@ namespace CXSN_normal {
     
 
     //% block="Init the motor as $x"
+    //% group="motor"
     export function motor_Init(x: PortNo) {
         cx_serial[x].init(pin_id[x][0], pin_id[x][2], 9600, Serial_mode._write);
     }
 
-    //% block="set %x the $no $dir rotation, speed is $speed"
+    //% block="set $x the $no $dir rotation, speed is $speed"
     //% inlineInputMode = inline
     //% speed.max=100 speed.min=0 speed.defl=100
+    //% group="motor"
     export function set_motor(x: PortNo, no: MotorNo, dir: MotorDir, speed: number) {
         if (no == MotorNo.motor_1) {
             cx_serial[x].write_numbers([0x5A, 0xA0, dir, speed]);
